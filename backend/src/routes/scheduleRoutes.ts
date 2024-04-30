7605bd9fa94f3d88c06cd34590047fb747f8a3bc
brmarketingexpert <164042568+brmarketingexpert@users.noreply.github.com>
2024-04-30 12:16:21 -0300

    Update scheduleRoutes.ts

--- backend/src/routes/scheduleRoutes.ts
+++ backend/src/routes/scheduleRoutes.ts
@@ -1,19 +1,26 @@
 import express from "express";
 import isAuth from "../middleware/isAuth";
-import tokenAuth from "../middleware/tokenAuth"; // Importação do middleware tokenAuth
+
 import * as ScheduleController from "../controllers/ScheduleController";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+
+const upload = multer(uploadConfig);
 
 const scheduleRoutes = express.Router();
 
-// Alteração da rota para utilizar tokenAuth em vez de isAuth
-scheduleRoutes.post("/schedules", tokenAuth, ScheduleController.store); // Utiliza tokenAuth
-
-// Mantendo as outras rotas como estavam, utilizando isAuth
 scheduleRoutes.get("/schedules", isAuth, ScheduleController.index);
+
+scheduleRoutes.post("/schedules", ScheduleController.store);
+
 scheduleRoutes.put("/schedules/:scheduleId", isAuth, ScheduleController.update);
+
 scheduleRoutes.get("/schedules/:scheduleId", isAuth, ScheduleController.show);
+
 scheduleRoutes.delete("/schedules/:scheduleId", isAuth, ScheduleController.remove);
-scheduleRoutes.post("/schedules/:id/media-upload", isAuth, ScheduleController.mediaUpload);
+
+scheduleRoutes.post("/schedules/:id/media-upload", isAuth, upload.array("file"), ScheduleController.mediaUpload);
+
 scheduleRoutes.delete("/schedules/:id/media-upload", isAuth, ScheduleController.deleteMedia);
 
 export default scheduleRoutes;
