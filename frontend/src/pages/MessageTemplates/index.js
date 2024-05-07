ba74bbcc0adacf9c78cd1042bceca9d5d7dc440d
Excarplex <164042568+brmarketingexpert@users.noreply.github.com>
2024-05-07 18:18:43 -0300

    Update index.js

--- frontend/src/pages/MessageTemplates/index.js
+++ frontend/src/pages/MessageTemplates/index.js
@@ -4,6 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography"; // Importação do componente Typography
 import { toast } from "react-toastify";
 
 import MainContainer from "../../components/MainContainer";
@@ -13,12 +14,13 @@ import Title from "../../components/Title";
 import api from "../../services/api";
 import { socketConnection } from "../../services/socket";
 
+// Estilos CSS customizados
 const useStyles = makeStyles((theme) => ({
   mainPaper: {
     flex: 1,
     padding: theme.spacing(1),
     overflowY: "scroll",
-    ...theme.scrollbarStyles,
+    ...theme.scrollbarStyles, // Estilos personalizados para scrollbars
   },
   messageContainer: {
     marginBottom: theme.spacing(2),
@@ -28,6 +30,9 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     marginBottom: theme.spacing(2),
   },
+  errorMessage: {
+    marginTop: theme.spacing(2), // Adicionando margem superior para separar da seção anterior
+  },
 }));
 
 const MessageTemplates = () => {
@@ -36,7 +41,8 @@ const MessageTemplates = () => {
   const [checkinTemplate, setCheckinTemplate] = useState("");
   const [feedbackTemplate, setFeedbackTemplate] = useState("");
   const [checkoutTemplate, setCheckoutTemplate] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // Estado para controlar o carregamento dos dados
+  const [error, setError] = useState(false); // Estado para controlar se ocorreu um erro na carga dos dados
 
   useEffect(() => {
     const storedCompanyId = localStorage.getItem("companyId");
@@ -44,31 +50,36 @@ const MessageTemplates = () => {
 
     const socket = socketConnection({ companyId: storedCompanyId });
 
+    // Subscreve-se a alterações nos ajustes da empresa via socket
     socket.on(`company-${storedCompanyId}-settings`, (data) => {
       if (data.action === "update") {
-        // Handle settings update if needed
+        // Lida com a atualização de ajustes, se necessário
       }
     });
 
     return () => {
-      socket.disconnect();
+      socket.disconnect(); // Desconecta o socket quando o componente é desmontado
     };
   }, []);
 
   useEffect(() => {
     if (companyId) {
       fetchTemplates();
+    } else {
+      setLoading(false); // Define o estado de carregamento como concluído para exibir a mensagem de erro
     }
   }, [companyId]);
 
+  // Função para buscar os templates de mensagens
   const fetchTemplates = async () => {
     try {
-      const xanoToken = process.env.XANO_TOKEN;
-      const headers = { Authorization: `Bearer ${xanoToken}` };
+      const xanoToken = process.env.XANO_TOKEN; // Obtém o token de acesso do ambiente
+      const headers = { Authorization: `Bearer ${xanoToken}` }; // Cabeçalho de autorização
       const templateRequests = [
-        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/checkin/{template_msgs_id}`, { headers }),
-        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/feedback/{template_msgs_id}`, { headers }),
-        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/checkout/{template_msgs_id}`, { headers }),
+        // Requisições assíncronas para obter os templates
+        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/checkin?template_msgs_id=${companyId}`, { headers }),
+        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/feedback?template_msgs_id=${companyId}`, { headers }),
+        api.get(`https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D/template_msgs/checkout?template_msgs_id=${companyId}`, { headers }),
       ];
 
       const [
@@ -77,32 +88,50 @@ const MessageTemplates = () => {
         checkoutResponse,
       ] = await Promise.all(templateRequests);
 
+      // Atualiza os estados com os templates obtidos
       setCheckinTemplate(checkinResponse.data.template);
       setFeedbackTemplate(feedbackResponse.data.template);
       setCheckoutTemplate(checkoutResponse.data.template);
     } catch (err) {
-      toast.error("Falha ao carregar os modelos de mensagens");
+      // Trata os erros durante a requisição
+      setError(true); // Define o estado de erro como verdadeiro
+      toast.error("Não foi possível sincronizar as mensagens. Entre em contato com o suporte.");
     } finally {
-      setLoading(false);
+      setLoading(false); // Define o estado de carregamento como concluído
     }
   };
 
+  // Função para atualizar um template
   const handleUpdateTemplate = async (endpoint, template) => {
     try {
-      const xanoToken = process.env.XANO_TOKEN;
-      const headers = { Authorization: `Bearer ${xanoToken}` };
-      const payload = { template_msgs_id: companyId, template };
-      await api.patch(endpoint, payload, { headers });
-      toast.success("Mensagem atualizada com sucesso!");
+      const xanoToken = process.env.XANO_TOKEN; // Obtém o token de acesso do ambiente
+      const headers = { Authorization: `Bearer ${xanoToken}` }; // Cabeçalho de autorização
+      const payload = { template_msgs_id: companyId, template }; // Payload da requisição
+      await api.patch(endpoint, payload, { headers }); // Requisição PATCH para atualizar o template
+      toast.success("Mensagem atualizada com sucesso!"); // Exibe mensagem de sucesso
     } catch (err) {
-      toast.error("Falha ao atualizar mensagem");
+      // Trata os erros durante a requisição
+      toast.error("Falha ao atualizar mensagem"); // Exibe mensagem de erro
     }
   };
 
+  // Se houver um erro na carga dos templates, exibe uma mensagem de erro na página
+  if (error) {
+    return (
+      <MainContainer>
+        <Typography variant="h6" color="error" className={classes.errorMessage}>
+          Não foi possível carregar os templates. Entre em contato com o suporte.
+        </Typography>
+      </MainContainer>
+    );
+  }
+
+  // Se estiver carregando os templates, exibe uma mensagem de carregamento na página
   if (loading) {
     return <div>Carregando...</div>;
   }
 
+  // Se não houver erro e os templates foram carregados, exibe os campos de templates na página
   return (
     <MainContainer>
       <MainHeader>
@@ -144,7 +173,7 @@ const MessageTemplates = () => {
               </Button>
             </div>
           </Grid>
-          {/* Feedback Template */}
+          {/* Seção de template de Feedback */}
           <Grid item>
             <div className={classes.messageContainer}>
               <TextField
@@ -179,7 +208,7 @@ const MessageTemplates = () => {
               </Button>
             </div>
           </Grid>
-          {/* Checkout Template */}
+          {/* Seção de template de Checkout */}
           <Grid item>
             <div className={classes.messageContainer}>
               <TextField
