32ce538e8389e7d8f6161ce6a47dee221cd4e8f6
Excarplex <164042568+brmarketingexpert@users.noreply.github.com>
2024-05-07 18:53:27 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,18 @@
+// frontend/src/services/api.js
+
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL,
-	withCredentials: true,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  withCredentials: true,
 });
 
 export const openApi = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL
+  baseURL: process.env.REACT_APP_BACKEND_URL
+});
+
+export const xanoApi = axios.create({
+  baseURL: "https://x8ki-letl-twmt.n7.xano.io/api:LP1Qco7D", // Altere para a URL correta do seu endpoint Xano
 });
 
 export default api;
